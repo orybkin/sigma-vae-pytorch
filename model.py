48f930c573b67dbeeb1ffaab6922cd439237454c
Oleh Rybkin <oleh.rybkin@gmail.com>
2020-06-06 20:17:20 -0400

    Initial VAE code from pytorch examples

---
+++ model.py
@@ -0,0 +1,101 @@
+import torch
+import torch.utils.data
+from torch import nn
+
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+class UnFlatten(nn.Module):
+    def __init__(self, n_channels):
+        super(UnFlatten, self).__init__()
+        self.n_channels = n_channels
+    
+    def forward(self, input):
+        size = int((input.size(1) // self.n_channels) ** 0.5)
+        return input.view(input.size(0), self.n_channels, size, size)
+
+
+class ConvVAE(nn.Module):
+    def __init__(self, device='cuda', img_channels=3, args=None):
+        super().__init__()
+        self.batch_size = args.batch_size
+        self.device = device
+        self.z_dim = 20
+        self.img_channels = img_channels
+        img_size = 28
+        filters_m = 32
+
+        ## Build network
+        self.encoder = self.get_encoder(self.img_channels, filters_m)
+
+        # output size depends on input image size, compute the output size
+        demo_input = torch.ones([1, self.img_channels, img_size, img_size])
+        h_dim = self.encoder(demo_input).shape[1]
+        print('h_dim', h_dim)
+        
+        # map to latent z
+        self.fc11 = nn.Linear(h_dim, self.z_dim)
+        self.fc12 = nn.Linear(h_dim, self.z_dim)
+
+        # decoder
+        self.fc2 = nn.Linear(self.z_dim, h_dim)
+        self.decoder = self.get_decoder(filters_m, self.img_channels)
+        
+    @staticmethod
+    def get_encoder(img_channels, filters_m):
+        return nn.Sequential(
+            nn.Conv2d(img_channels, filters_m, (3, 3), stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(filters_m, 2 * filters_m, (4, 4), stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(2 * filters_m, 4 * filters_m, (5, 5), stride=2, padding=2),
+            nn.ReLU(),
+            Flatten()
+        )
+
+    @staticmethod
+    def get_decoder(filters_m, out_channels):
+        return nn.Sequential(
+            UnFlatten(4 * filters_m),
+            nn.ConvTranspose2d(4 * filters_m, 2 * filters_m, (6, 6), stride=2, padding=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(2 * filters_m, filters_m, (6, 6), stride=2, padding=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(filters_m, out_channels, (5, 5), stride=1, padding=2),
+            nn.Sigmoid(),
+        )
+        
+    def encode(self, x):
+        h = self.encoder(x)
+        return self.fc11(h), self.fc12(h)
+    
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+    
+    def decode(self, z):
+        return self.decoder(self.fc2(z))
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+    
+    def sample(self, n):
+        sample = torch.randn(n, self.z_dim).to(self.device)
+        return self.decode(sample)
+    
+    def loss_function(self, recon_x, x, mu, logvar):
+        rec = torch.nn.MSELoss()(recon_x, x)
+        
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        kl = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        
+        return rec, kl
---
+++ train_vae.py
@@ -0,0 +1,115 @@
+from __future__ import print_function
+
+import argparse
+import os
+
+import torch
+from torch import optim
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+from tqdm import tqdm
+
+from model import ConvVAE
+
+""" This script is an example of Sigma VAE training in PyTorch. The code was adapted from:
+https://github.com/pytorch/examples/blob/master/vae/main.py """
+
+## Arguments
+parser = argparse.ArgumentParser(description='VAE MNIST Example')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--log_dir', type=str, default='test', metavar='N', required=True)
+args = parser.parse_args()
+
+## Cuda
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+device = torch.device("cuda" if args.cuda else "cpu")
+
+## Dataset
+transform = transforms.Compose([transforms.Resize((28, 28)), transforms.ToTensor()])
+train_dataset = datasets.SVHN('../../data', split='train', download=True, transform=transform)
+test_dataset = datasets.SVHN('../../data', split='train', transform=transform)
+kwargs = {'num_workers': 10, 'pin_memory': True} if args.cuda else {}
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
+
+## Logging
+os.makedirs('vae_logs/{}'.format(args.log_dir), exist_ok=True)
+summary_writer = SummaryWriter(log_dir='vae_logs/' + args.log_dir, purge_step=0)
+
+## Build Model
+model = ConvVAE(device, 3, args).to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        
+        # Run VAE
+        recon_batch, mu, logvar = model(data)
+        # Compute loss
+        rec, kl = model.loss_function(recon_batch, data, mu, logvar)
+        
+        total_loss = rec + kl
+        total_loss.backward()
+        train_loss += total_loss.item()
+        optimizer.step()
+        
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tMSE: {:.6f}\tKL: {:.6f}\tlog_sigma: {:f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                rec.item() / len(data),
+                kl.item() / len(data),
+                0))
+            
+    train_loss /=  len(train_loader.dataset)
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+        epoch, train_loss))
+    summary_writer.add_scalar('train/elbo', train_loss, epoch)
+    summary_writer.add_scalar('train/rec', rec.item() / len(data), epoch)
+    summary_writer.add_scalar('train/kld', kl.item() / len(data), epoch)
+
+
+def test(epoch):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, _) in enumerate(tqdm(test_loader)):
+            data = data.to(device)
+            recon_batch, mu, logvar = model(data)
+            # Pass the second value from posthoc VAE
+            rec, kl = model.loss_function(recon_batch, data, mu, logvar)
+            test_loss += rec + kl
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n], recon_batch.view(args.batch_size, -1, 28, 28)[:n]])
+                save_image(comparison.cpu(), 'vae_logs/{}/reconstruction_{}.png'.format(args.log_dir, str(epoch)), nrow=n)
+                
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+    summary_writer.add_scalar('test/elbo', test_loss, epoch)
+
+
+if __name__ == "__main__":
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        test(epoch)
+        with torch.no_grad():
+            sample = model.sample(64).cpu()
+            save_image(sample.view(64, -1, 28, 28),
+                       'vae_logs/{}/sample_{}.png'.format(args.log_dir, str(epoch)))
+        summary_writer.file_writer.flush()
+        
+    torch.save(model.state_dict(), 'vae_logs/{}/checkpoint_{}.pt'.format(args.log_dir, str(epoch)))
